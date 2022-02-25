034e40732da7e599a0e580aa0d91565355f7a4e5
kostashassouros <kostas.hassouros@gmail.com>
2022-02-26 09:58:09 +1030

    Added Filter Nav to Teams Page

--- src/views/Teams/Teams.js
+++ src/views/Teams/Teams.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
+import {
+    Container,
+    Box,
+    Flex
+} from "@chakra-ui/react";
 
 const Teams = () => {
-    return <Box>Test</Box>;
+    return (
+        <Container maxW="container.lg">
+            <Box bg="white" border="1px" borderColor="gray.100" rounded="full" shadow="lg">
+                <Flex p={6} px={8} alignItems="center" justifyContent="space-between">
+                    <Box>Test</Box>
+                </Flex>
+            </Box>
+        </Container>
+    );
 }
 
 export default Teams;
