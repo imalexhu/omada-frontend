287ce2f5b1ba3805b36bb4e47e240e68a7a8c085
Che Kambouris <chekambouris@gmail.com>
2022-02-26 10:11:24 +1030

    Create component file

---
+++ src/components/ProjectDetails/ProjectInfoModal.js
@@ -0,0 +1,27 @@
+import { React, useEffect, useState } from "react";
+import { BsStack } from "react-icons/bs"
+import {
+	Box,
+	Flex,
+	Heading,
+	Icon,
+	Image,
+	Spacer,
+	Text,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalFooter,
+	ModalBody,
+	ModalCloseButton,
+} from "@chakra-ui/react";
+
+
+const ProjectInfoModal = ({ isOpen, project, creator }) => {
+	return (
+		<Box>ProjectInfoModal</Box>
+	);
+}
+
+export default ProjectInfoModal;
