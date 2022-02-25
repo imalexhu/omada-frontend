69b5cd423d0da8e9fa3e761a2f2d26e52e379ced
Che Kambouris <chekambouris@gmail.com>
2022-02-26 09:32:30 +1030

    Add creator and a few other things

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,15 @@
-import React from "react";
-
-import { Box, Flex, Heading, Icon, Text } from "@chakra-ui/react";
+import { React, useEffect, useState } from "react";
+import { BsStack } from "react-icons/bs"
+import {
+	Box,
+	Flex,
+	Heading,
+	Icon,
+	Image,
+	Spacer,
+	Text
+} from "@chakra-ui/react";
+import axios from "axios"
 
 const difficultyColor = {
 	"Easy": "limegreen",
@@ -8,15 +17,35 @@ const difficultyColor = {
 	"Hard": "red",
 };
 
-const ProjectCard = ({ title, description, difficulty, availableRoles, techInUse }) => {
+
+const ProjectCard = ({ title,
+						creator,
+						description,
+						difficulty,
+						availableRoles,
+						techInUse }) => {
+	const [profile, setProfile] = useState({});
+	const getGithubUser = async (username) => {
+		let r = await axios.get(`https://api.github.com/users/` + username);
+		setProfile(r.data);
+	}
+	useEffect(() => getGithubUser(creator.githubID))
 	return (
 		<Box position="relative" borderRadius="3xl" boxShadow="0px 4px 10px rgba(0,0,0,0.15)" padding={4}>
-			<Heading marginBottom={3}>{title}</Heading>
-			<Box position="absolute" right="5" top="5"
-				paddingBlock="0.5" paddingInline="3"
-				bg={difficultyColor[difficulty]} borderRadius="2xl">
-				<Text fontWeight="bold" color="white">{difficulty}</Text>
-			</Box>
+			<Flex justifyContent="space-between" >
+				<Flex>
+					<Heading marginBottom={3}>{title}</Heading>
+					<Spacer w="2"/>
+					{/* <Image fit="scale-down" src={profile?.avatar_url}/> */}
+					<Text alignSelf="center" color="gray">{creator.profileName}</Text>
+				</Flex>
+				<Box>
+					<Text paddingBlock="0.5" paddingInline="3" borderRadius="2xl"
+						bg={difficultyColor[difficulty]} fontWeight="bold" color="white">
+							{difficulty}
+					</Text>
+				</Box>
+			</Flex>
 			<Text color="gray" noOfLines={2}>{description}</Text>
 			<Flex justifyContent="space-between" paddingTop="3">
 				<Text>{techInUse.join(', ')}</Text>
