7cbd1c3346e25e51e2000582d2264528e1e22a79
PatrickS136 <tricksumarli@gmail.com>
2022-02-26 17:59:30 +1030

    UNI-21

--- src/views/UserOnboarding/form/Email.js
+++ src/views/UserOnboarding/form/Email.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Box, HStack, Input, Button } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Input,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Spinner,
+} from "@chakra-ui/react";
 
 const Email = () => {
+  let navigate = useNavigate();
+  const { isOpen, onOpen } = useDisclosure();
+
+  const handleSubmit = () => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+    onOpen();
+  };
+
   return (
     <React.Fragment>
       <Box
@@ -18,9 +41,25 @@ const Email = () => {
       <HStack w="100%" spacing={2}>
         <Input p={3} placeholder="Email" size="lg" bg="white" rounded="full" />
       </HStack>
-      <Button colorScheme="messenger" size="lg" p={5}>
+      <Button colorScheme="messenger" size="lg" p={5} onClick={handleSubmit}>
         Submit
       </Button>
+      <Modal blockScrollOnMount={false} isOpen={isOpen}>
+        <ModalOverlay />
+        <ModalContent alignItems={"center"} mt={250}>
+          <ModalHeader>Automating Integration</ModalHeader>
+          <ModalBody>
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+              mb={4}
+            />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </React.Fragment>
   );
 };
