b2df06426fdcc8ff25b9a4d354640ea95a9ee3af
kostashassouros <kostas.hassouros@gmail.com>
2022-02-26 13:30:01 +1030

    Working on form state

--- src/views/CreateTeam/CreateTeam.js
+++ src/views/CreateTeam/CreateTeam.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
     TeamName,
@@ -15,33 +15,37 @@ import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
 
 const CreateTeam = () => {
     const [page, setPage] = useState(1);
+    const [formData, setFormData] = useState({
+        teamName: "",
+        teamDescription: "",
+    });
 
-    const nextForm = () => setPage(page => page + 1);
+    const nextForm = () => setPage((page) => page + 1);
     const prevForm = () => {
-        if (page === 1) {
-            return;
-        }
+        if (page === 1) return;
 
-        setPage(page => page - 1)
+        setPage((page) => page - 1)
     };
-
+    
     return (
         <React.Fragment>
             <Flex h="100vh" bg="#F5F5F6" alignItems="center">
                 <Container maxW="container.md">
-                    <VStack h="10vh" spacing={6} mb={8}>
-                        {page === 1 && <TeamName page={page} setPage={setPage} />}
-                        {page === 2 && <TeamDescription page={page} setPage={setPage} />}
+                    <VStack h="10vh" spacing={6}>
+                        {page === 1 && <TeamName />}
+                        {page === 2 && <TeamDescription />}
 
-                        <HStack pt={6} spacing={8}>
-                            <IconButton
-                                colorScheme="messenger"
-                                aria-label="Previous Form"
-                                size="lg"
-                                rounded="full"
-                                onClick={prevForm}
-                                icon={<BsArrowLeft size={22} />}
-                            />
+                        <HStack pt={4} spacing={8}>
+                            {page > 1 && 
+                                <IconButton
+                                    colorScheme="messenger"
+                                    aria-label="Previous Form"
+                                    size="lg"
+                                    rounded="full"
+                                    onClick={prevForm}
+                                    icon={<BsArrowLeft size={22} />}
+                                />
+                            }
                             <IconButton
                                 colorScheme="messenger"
                                 aria-label="Next Form"
