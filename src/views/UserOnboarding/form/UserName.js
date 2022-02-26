79e686d43412f3b4bb89ed2646ba3a0e490a7392
PatrickS136 <tricksumarli@gmail.com>
2022-02-26 21:58:32 +1030

    onboarding form update

--- src/views/UserOnboarding/form/UserName.js
+++ src/views/UserOnboarding/form/UserName.js
@@ -1,30 +1,30 @@
 import React from "react";
+import FadeIn from "react-fade-in";
 
-import { Box, HStack, Input } from "@chakra-ui/react";
+import { VStack, Box, Input } from "@chakra-ui/react";
 
-const UserName = () => {
+const UserName = ({ handleFormData, values }) => {
   return (
-    <React.Fragment>
-      <Box
-        fontSize={{ base: "2xl", md: "3xl" }}
-        fontWeight={400}
-        color="gray.700"
-      >
-        What's Your{" "}
-        <Box as="span" fontWeight={700} color="#0053CD">
-          User Name?
+    <FadeIn>
+      <VStack w="100%" spacing={8} alignItems="center">
+        <Box
+          fontSize={{ base: "2xl", md: "3xl" }}
+          fontWeight={400}
+          color="gray.700"
+        >
+          What's Your{" "}
+          <Box as="span" fontWeight={700} color="#0053CD">
+            Team Name?
+          </Box>
         </Box>
-      </Box>
-      <HStack w="100%" spacing={2}>
         <Input
-          p={3}
-          placeholder="User Name"
+          value={values["userName"]}
+          placeholder="Team Name"
           size="lg"
-          bg="white"
-          rounded="full"
+          onChange={handleFormData("userName")}
         />
-      </HStack>
-    </React.Fragment>
+      </VStack>
+    </FadeIn>
   );
 };
 
