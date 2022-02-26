d6ef33e053ec0c8534052d5b21c563d12d9759bd
Che Kambouris <chekambouris@gmail.com>
2022-02-26 11:27:28 +1030

    Open modal

--- src/components/ProjectDetails/ProjectInfoModal.js
+++ src/components/ProjectDetails/ProjectInfoModal.js
@@ -18,9 +18,25 @@ import {
 } from "@chakra-ui/react";
 
 
-const ProjectInfoModal = ({ isOpen, project, creator }) => {
+const ProjectInfoModal = ({ isOpen, onClose, project, creator }) => {
 	return (
-		<Box>ProjectInfoModal</Box>
+		<Modal isOpen={isOpen} onClose={onClose}>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>Modal Title</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+					<Lorem count={2} />
+				</ModalBody>
+
+				<ModalFooter>
+					<Button colorScheme='blue' mr={3} onClick={onClose}>
+						Close
+					</Button>
+					<Button variant='ghost'>Secondary Action</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
 	);
 }
 
