d17f58592764348502aaa558af96d808a9a7cfc4
kostashassouros <kostas.hassouros@gmail.com>
2022-02-27 12:38:04 +1030

    Home page completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import UserOnboarding from "./views/UserOnboarding/UserOnboarding"
-import { Header } from "./components/Layout";
+
+import { Header, Footer } from "./components/Layout";
+import {
+	CreateTeam,
+	Home,
+	CreateURL,
+	UserOnboarding
+} from "./views";
 import UserProfile from "./components/Users/UserProfile";
-import { CreateTeam } from "./views";
-import Home from "./views/Home/Home";
 import Members from "./components/Users/Members";
-import CreateURL from "./views/CreateURL/CreateURL";
-import UserOnboarding from "./views/UserOnboarding/UserOnboarding";
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/createteam" element={<CreateTeam />} />
-        <Route path="/profile" element={<UserProfile user={0} />} />
-        <Route path="/members" element={<Members />} />
-        <Route path="/createurl" element={<CreateURL />} />
-        <Route path="/user_onboarding" element={<UserOnboarding />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/createteam" element={<CreateTeam />} />
+				<Route path="/profile" element={<UserProfile user={0} />} />
+				<Route path="/members" element={<Members />} />
+				<Route path="/createurl" element={<CreateURL />} />
+				<Route path="/user_onboarding" element={<UserOnboarding />} />
+			</Routes>
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,6 @@
-// import { default as Teams } from "./Teams/Teams";
 import { default as CreateTeam } from "./CreateTeam/CreateTeam";
+import { default as Home } from "./Home/Home";
+import { default as CreateURL } from "./CreateURL/CreateURL";
+import { default as UserOnboarding } from "./UserOnboarding/UserOnboarding";
 
-export { CreateTeam }
+export { CreateTeam, Home, CreateURL, UserOnboarding }
